01818354acbf474479822d19c58fa2b2f748437b
hypothermic <admin@hypothermic.nl>
2019-12-11 00:37:39 +0100

    Update met atomic i.p.v. critical block voor betere prestaties
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- main.c
+++ main.c
@@ -1,81 +1,89 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <time.h>
 #include <stdarg.h>
 #include <omp.h>
 #include <gmp.h>
+#include <malloc.h>
 
-#define DEBUG 0
+//#define DEBUG
 
-const __uint16_t EXPECTED_RESULT = 777;
-const __uint64_t mod             = 9999999998987;
-const __uint8_t  root            = 5;
-const __uint32_t step            = 25000;
+const __uint32_t EXPECTED_RESULT = 777;
+const __uint64_t MOD             = 9999999998987;
+const __uint8_t  BASE            = 5;
+const __uint32_t STEP            = 200000;
 
 __uint64_t lpow(__uint64_t uroot, __uint64_t exp);
-void flog(FILE* stream, const char* format, ...);
+static void _logf(FILE* dest, const char* format, ...);
 
-int main() {
-    __uint8_t    lock          = 0,
-                 local_lock    = 0;
-    __uint64_t   current       = 0,
-                 local_current = 0;
-    __uint64_t   last_step     = time(NULL);
+int main(int argc, char* argv[]) {
+    __uint8_t           lock          = 0,
+                        local_lock    = 0;
+    __uint64_t          current       = 0,
+                        local_current = 0;
+    __uint64_t          last_step     = time(NULL);
+    const time_t        start_time    = last_step;
 
-    static mpz_t result,
-                 intermediate;
+    static mpz_t        result,
+                        intermediate;
+    static const mpz_t  expected;
     #pragma omp threadprivate(result, intermediate)
 
+    mpz_init(result);
+    mpz_init(intermediate);
+    mpz_init(expected);
 
-    flog(stdout, "Start met brute-force\n");
+    mpz_set_d(expected, EXPECTED_RESULT);
 
-    #pragma omp parallel private (local_current, local_lock)
+    _logf(stdout, "Start met brute-force\n");
+
+    #pragma omp parallel /*num_threads(8)*/ private (local_current, local_lock)
     {
         mpz_init(result);
         mpz_init(intermediate);
 
         while (!local_lock) {
 
-            #pragma omp critical
+            #pragma omp atomic capture
             {
-
                 local_current = current;
                 current++;
-
-                local_lock = lock;
             }
 
-            mpz_ui_pow_ui(intermediate, root, local_current);
-            mpz_mod_ui(result, intermediate, mod);
+            #pragma omp atomic read
+            local_lock = lock;
 
-            if (DEBUG) {
+            mpz_ui_pow_ui(intermediate, BASE, local_current);
+            mpz_fdiv_r_ui(result, intermediate, MOD);
+
+            #ifdef DEBUG
                 sleep(1);
 
                 #pragma omp critical
-                gmp_fprintf(stdout, "Thread %d op count %lu (calc %Zd)\n", omp_get_thread_num(), local_current, result);
-            }
+                _logf(stderr, "Thread %d op count %lu (calc %Zd, %Zd)\n", omp_get_thread_num(), local_current, result, intermediate);
+            #endif
 
-            if (local_current % step == 0) {
+            if (local_current % STEP == 0) {
                 #pragma omp critical
                 {
-                    flog(stdout, "%d stappen voltooid in de laatste %lu seconden (%d totaal)", step,
-                         time(NULL) - last_step,
-                         current);
+                    _logf(stdout, "%d combinaties geprobeerd in de laatste %lu seconden (%d totaal)",
+                          STEP, time(NULL) - last_step, current);
                     last_step = time(NULL);
                 }
             }
 
-            if (mpz_cmp_ui(result, EXPECTED_RESULT) == 0) {
+            if (mpz_cmp(result, expected) == 0) {
                 #pragma omp atomic update
                 lock++;
 
                 #pragma omp critical
-                gmp_fprintf(stdout, "[%lu] Key gevonden: %Zd\n", time(NULL), result);
+                _logf(stdout, "Key gevonden na %d seconden: %lu\n", start_time - last_step, local_current);
             }
         }
     }
 
-    flog(stdout, "Alle threads zijn gestopt.\n");
+    _logf(stdout, "Alle threads zijn gestopt.\n");
     return 0;
 }
 
@@ -96,15 +104,29 @@ __uint64_t lpow(__uint64_t uroot, __uint64_t exp) {
 }
 
 /**
- * Log naar een stream met een mooi tijdstip er voor
+ * Log naar een stream met een mooi tijdstip er voor.<br />
+ * <br />
+ * Support voor GMP format is aanwezig maar waarden met deze extensies worden niet gecheckt.
  */
-void flog(FILE* file, const char* format, ...) {
+static void _logf(FILE* dest, const char* format, ...) {
     va_list args;
+    time_t  ltime;
+    char*   lstr;
+    size_t  lstrlen;
+
     va_start(args, format);
 
-    fprintf(file, "[%lu] ", (__uint64_t) time(NULL));
-    vfprintf(file, format, args);
-    fprintf(file, "\n");
+    ltime = time(NULL);
+    lstr = asctime(localtime(&ltime));
+    lstrlen = strlen(lstr);
+    if (lstrlen > 0 && lstr[lstrlen-1] == '\n') {
+        lstr[--lstrlen] = '\0';
+    }
+
+    fprintf(dest, "[%s] ", lstr);
+
+    gmp_vfprintf(dest, format, args);
+    fprintf(dest, "\n");
 
     va_end(args);
 }
